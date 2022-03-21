ba2921a29613103e8f5e0a3d18bf6e69da8b38cf
emre-bulbul <emrevlog0@gmail.com>
2022-03-21 04:24:05 +0300

    Angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
+import {Event} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles:[`h3{ color: dodgerblue}`]
 })
+
 export class AppComponent {
+  allowAddServer : boolean = false;
   title = 'AngularProject';
+  serverName :string = "";
+  name: any;
+  serverCreationStatus:string = "server was not created !";
+  userName:string ='';
+  serverCreated: boolean=false;
+  servers = ["testServer 1","testServer 2"];
+  showSecret: boolean = false;
+  logs  :number[]= [];
+
+  constructor() {
+
+    setTimeout(()=> {
+      this.allowAddServer = true;
+    },2000)
+  }
+  onCreateServer(){
+    this.servers.push("new server");
+    this.serverCreated =true;
+    this.serverCreationStatus = "server was created!"+this.serverName;
+  }
+
+  onUpdateServerName($event: any  ) {
+
+      this.serverName =$event.target.value;
+
+  }
+
+  enableOrDisable(): boolean {
+    if(this.userName ==''){
+      return false;
+    }
+    return true;
+  }
+
+  resetUserName() {
+    this.userName = "";
+  }
+
+  incrementLog() {
+    this.logs.push(this.logs.length+1);
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {ServerComponent} from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
